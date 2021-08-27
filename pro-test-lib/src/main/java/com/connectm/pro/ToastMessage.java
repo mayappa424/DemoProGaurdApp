68e6145bfb7e99349eebedf6ff7ee148efd1c6c1
Mayappa SB <mayappa.sb@connectm.com>
2021-08-27 20:40:33 +0530

    second commit

--- pro-test-lib/src/main/java/com/connectm/pro/ToastMessage.java
+++ pro-test-lib/src/main/java/com/connectm/pro/ToastMessage.java
@@ -3,8 +3,11 @@ package com.connectm.pro;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.annotation.Keep;
+
 public class ToastMessage {
 
+    @Keep
     public void showToastToMe(Context context, String message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
